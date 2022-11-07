40013cadf8f7f717746dfe54343f44c8b10763e3
Andrés Torres <andres.torressandoval@hotmail.com>
2022-11-07 18:18:23 -0500

    Tercer desafío

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 const Container =require("./Contenedor.js")
 const productos = new Container('./productos.json')
+const express = require('express')
+const moment = require('moment')
 
+const app = express();
+
+const PORT = 8080
+
+//Instanciamos la clase
+const archivo = new Container('productos.json')
+
+const visitas = {
+    productos: 0,
+    fecha_ingreso_productos: '',
+    prod_random: 0,
+    fecha_ingreso_productos_random: ''
+
+}
+
+app.get('/productos', async (req, res)=>{
+    visitas.productos++
+    visitas.fecha_ingreso_productos = moment().format('MMMM Do YYYY, h:mm:ss a');
+    const prods = await archivo.getAll()
+    res.send({productos: prods})
+} )
+
+app.get('/random', async (req, res)=>{
+    visitas.prod_random++
+    visitas.fecha_ingreso_productos_random = moment().format('MMMM Do YYYY, h:mm:ss a');
+    const prods = await archivo.getAll()
+    const random = parseInt(Math.random()*prods.length)
+    res.send({Productos:prods[random]})
+})
+
+app.get('/visitas', (req, res)=>{
+    res.send({ visitas })
+
+})
 async function ejecutar(){
     const objeto3 ={
         nombre : 'Aceite facial piel seca',
@@ -17,4 +53,8 @@ async function ejecutar(){
 
     await productos.deleteById(1)
 }
-ejecutar()
+
+const server = app.listen(PORT, ()=>{
+    console.log(`Servidor corriendo en el puerto: ${PORT}`);
+
+})
