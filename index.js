06bf06a1ee8469953506adde1335920dddfe4c1b
Andrés Torres <andres.torressandoval@hotmail.com>
2022-11-07 18:50:22 -0500

    tercer desafio

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const Container =require("./Contenedor.js")
-const productos = new Container('./productos.json')
+const Container = require("./Contenedor.js")
 const express = require('express')
 const moment = require('moment')
 
@@ -15,46 +14,29 @@ const visitas = {
     fecha_ingreso_productos: '',
     prod_random: 0,
     fecha_ingreso_productos_random: ''
-
 }
 
-app.get('/productos', async (req, res)=>{
+app.get('/productos', async (req, res) => {
     visitas.productos++
     visitas.fecha_ingreso_productos = moment().format('MMMM Do YYYY, h:mm:ss a');
     const prods = await archivo.getAll()
-    res.send({productos: prods})
-} )
+    res.send({ productos: prods })
+})
 
-app.get('/random', async (req, res)=>{
+app.get('/random', async (req, res) => {
     visitas.prod_random++
     visitas.fecha_ingreso_productos_random = moment().format('MMMM Do YYYY, h:mm:ss a');
     const prods = await archivo.getAll()
-    const random = parseInt(Math.random()*prods.length)
-    res.send({Productos:prods[random]})
+    const random = parseInt(Math.random() * prods.length)
+    res.send({ Productos: prods[random] })
 })
 
-app.get('/visitas', (req, res)=>{
+app.get('/visitas', (req, res) => {
     res.send({ visitas })
 
 })
-async function ejecutar(){
-    const objeto3 ={
-        nombre : 'Aceite facial piel seca',
-        precio: 200
-    }
-    const objeto2 ={
-        nombre: 'Aceite facial piel grasa',
-        precio: 300
-    }
-    const objeto1 ={
-        nombre: 'Aceite facial piel mixta',
-        precio:  400
-    }
 
-    await productos.deleteById(1)
-}
-
-const server = app.listen(PORT, ()=>{
+const server = app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto: ${PORT}`);
 
 })
