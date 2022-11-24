341fa105f4c286833b001c361684fd3a40938c18
Andrés Torres <andres.torressandoval@hotmail.com>
2022-11-23 20:31:04 -0500

    desafio plantilla con hbs

--- src/views/index.js
+++ src/views/index.js
@@ -4,22 +4,22 @@ const handlebars = require('express-handlebars')
 
 const views = function(app, router, productosService) {
 
-    app.set('view engine', 'ejs');
-    app.set('views', path.join(__dirname, 'ejs'));
+    // app.set('view engine', 'ejs');
+    // app.set('views', path.join(__dirname, 'ejs'));
 
     // app.set('view engine', 'pug');
     // app.set('views', path.join(__dirname, 'pug'));
 
-    // const hbs = require('express-handlebars').create({
-    //     layoutsDir: path.join(__dirname, "hbs"),
-    //     partialsDir: path.join(__dirname, "hbs"),
-    //     defaultLayout: 'inicio',
-    //     extname: 'hbs'
-    // });
+    const hbs = require('express-handlebars').create({
+        layoutsDir: path.join(__dirname, "hbs"),
+        partialsDir: path.join(__dirname, "hbs"),
+        defaultLayout: 'inicio',
+        extname: 'hbs'
+    });
 
-    // app.engine('hbs', hbs.engine)
-    // app.set('view engine', 'hbs');
-    // app.set('views', path.join(__dirname, 'hbs'))
+    app.engine('hbs', hbs.engine)
+    app.set('view engine', 'hbs');
+    app.set('views', path.join(__dirname, 'hbs'))
 
     router.get('/', (req, res) => {
         res.render('inicio', { productos: productosService.getAll() });
