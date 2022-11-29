644b61453732bca86d1009de4a5860ad004b7283
Andrés Torres <andres.torressandoval@hotmail.com>
2022-11-28 20:05:22 -0500

    sockets

--- src/server.js
+++ src/server.js
@@ -4,8 +4,24 @@ const app = express();
 const PORT = 8080
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
+const { Server: IO } = require('socket.io')
+const { Server: HttpServer } = require('http')
+const httpServer = new HttpServer(app)
+const io = new IO(httpServer)
+
 // app.use(express.static('public'))
 app.use(router(app))
+
+// Sockets init
+io.on('connection', socket => {
+    console.log('Nuevo cliente conectado!!');
+
+    //on new-product emit products
+    socket.on('new-product')
+        io.sockets.emit('productos')
+})
+
+
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto: ${PORT}`);
--- src/views/index.js
+++ src/views/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 var path = require('path')
 const handlebars = require('express-handlebars')
+const io = require('socket.io-client')
 
 const views = function(app, router, productosService) {
-
+    let socket;
     // app.set('view engine', 'ejs');
     // app.set('views', path.join(__dirname, 'ejs'));
 
@@ -23,11 +24,20 @@ const views = function(app, router, productosService) {
 
     router.get('/', (req, res) => {
         res.render('inicio', { productos: productosService.getAll() });
+        if(!socket) socket = io.connect();
     })
 
     router.post('/productos', (req, res) => {
         const prod = req.body;
         productosService.save(prod);
+        res.redirect('/');
+
+        //emit new-producto
+        socket && socket.emit('new-product')
+    })
+
+    // On products
+    socket && socket.on('productos', () => {
         res.redirect('/');
     })
 }
